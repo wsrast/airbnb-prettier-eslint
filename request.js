c507cdb79f1b282fbdc1b1226ef08c2a84f192b6
Wesley Rast <wesley.rast@paya.com>
2019-03-07 16:38:02 -0500

    Update cwd for postinstall script
    There's an issue with the postinstall script,
    even when using plain npm to run the install. It
    doesn't seem to run at all, but I'm suspicious that
    the script is adding the files to some other folder.

--- request.js
+++ request.js
@@ -1,11 +1,14 @@
 const request = require('request');
+const path = require('path');
 const fs = require('fs');
 
+const cwd = process.cwd();
+console.log(`cwd**: ${cwd}`);
 const filesPath =
 	'https://raw.githubusercontent.com/wsrast/airbnb-prettier-eslint/master/';
 const fileNames = ['.editorconfig', '.eslintrc.json', '.prettierrc'];
 
 fileNames.forEach(fileName => {
 	const url = `${filesPath}${fileName}`;
-	request(url).pipe(fs.createWriteStream(fileName));
+	request(url).pipe(fs.createWriteStream(path.join(cwd, fileName)));
 });
