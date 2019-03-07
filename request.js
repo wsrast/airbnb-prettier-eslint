7920a04ba77a2b55fdbd2c53bd1da20e709a5255
Wesley Rast <wesley.rast@paya.com>
2019-03-07 17:01:48 -0500

    Fix relative path for file copies

--- request.js
+++ request.js
@@ -3,12 +3,20 @@ const path = require('path');
 const fs = require('fs');
 
 const cwd = process.cwd();
-console.log(`cwd**: ${cwd}`);
 const filesPath =
 	'https://raw.githubusercontent.com/wsrast/airbnb-prettier-eslint/master/';
 const fileNames = ['.editorconfig', '.eslintrc.json', '.prettierrc'];
 
 fileNames.forEach(fileName => {
 	const url = `${filesPath}${fileName}`;
-	request(url).pipe(fs.createWriteStream(path.join(cwd, fileName)));
+	/* The relative hops to parent folders are necessary because
+	 * the postinstall script is executing relative to this package,
+	 * not the parent package.
+	 * So, test for the existence of the node_modules folder in the
+	 * cwd, which means it's being installed, and not worked on. */
+	const relativePath = cwd.indexOf('node_modules')
+		? path.resolve(cwd, '..', '..', '..', fileName)
+		: path.resolve(cwd, fileName);
+
+	request(url).pipe(fs.createWriteStream(path.resolve(relativePath)));
 });
