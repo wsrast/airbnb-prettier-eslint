78063539f9424b0b694548ae628ac7419fde0342
Wesley Rast <wesley.rast@paya.com>
2019-03-13 14:59:36 -0400

    Add ESLint jest plugin and rule mods
    I'm adding the trailingCommas setting for prettier,
    as there's good reason to have this there: easy
    additions/removals and it cleans up git diff messages.
    
    I'm also adding the ESLint plugin for Jest to try and
    take care of the "describe", "it" and "expect" functions
    being highlighted as errors in the parent project.

--- request.js
+++ request.js
@@ -7,6 +7,9 @@ const filesPath =
 	'https://raw.githubusercontent.com/wsrast/airbnb-prettier-eslint/master/';
 const fileNames = ['.editorconfig', '.eslintrc.json', '.prettierrc'];
 
+const projRoot = cwd.split('node_modules')[0];
+console.log(`Installing to project folder: `, projRoot);
+
 fileNames.forEach(fileName => {
 	const url = `${filesPath}${fileName}`;
 	/* The relative hops to parent folders are necessary because
@@ -14,9 +17,9 @@ fileNames.forEach(fileName => {
 	 * not the parent package.
 	 * So, test for the existence of the node_modules folder in the
 	 * cwd, which means it's being installed, and not worked on. */
-	const relativePath = cwd.indexOf('node_modules')
-		? path.resolve(cwd, '..', '..', '..', fileName)
-		: path.resolve(cwd, fileName);
+	const relativePath = path.join(projRoot, fileName);
 
-	request(url).pipe(fs.createWriteStream(path.resolve(relativePath)));
+	if (projRoot !== cwd) {
+		request(url).pipe(fs.createWriteStream(path.resolve(relativePath)));
+	}
 });
