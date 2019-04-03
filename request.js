2d5aeaac0296da1d9186ecb761bb45778b1ce83c
Wesley Rast <wesley.rast@paya.com>
2019-04-03 13:59:02 -0400

    Update all packages and add testing
    Updating all packages and adding new testing setup
    files for Jest for convenience.

--- request.js
+++ request.js
@@ -19,13 +19,15 @@ fileNames.forEach(fileName => {
 	 * cwd, which means it's being installed, and not worked on. */
 	const relativePath = path.join(projRoot, fileName);
 
-	// TODO: check to see if there's already a file at this location!
-	//	if there is, then don't overwrite it. Very annoying to deal
-	//	with git changes!
-	// Maybe: this should check to see whether or not the contents
+	// TODO: this should check to see whether or not the contents
 	//	of the new file would be different, and if so, overwrite?
 
-	if (projRoot !== cwd) {
-		request(url).pipe(fs.createWriteStream(path.resolve(relativePath)));
+	// check to see if there's already a file at this location!
+	//	if there is, then don't overwrite it. Very annoying to deal
+	//	with git changes!
+	if (!fs.existsSync(relativePath)) {
+		if (projRoot !== cwd) {
+			request(url).pipe(fs.createWriteStream(path.resolve(relativePath)));
+		}
 	}
 });
